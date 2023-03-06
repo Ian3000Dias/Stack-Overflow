08fc72ba0bceac79c3b80e0c695130ae0de83e9b
Ian3000Dias <iandias347@gmail.com>
2023-03-06 13:58:23 +0530

    client changes

--- client/src/pages/Questions/QuestionDetails.jsx
+++ client/src/pages/Questions/QuestionDetails.jsx
@@ -74,7 +74,7 @@ const QuestionDetails = () => {
     const dispatch = useDispatch()
     const User = useSelector((state)=>(state.currentUserReducer))
     const location = useLocation()
-    const url = 'http://localhost:3000'
+    const url = 'https://stack-overflow-7j9j.onrender.com'
     
 
     const handlePostAns = (e, answerLength) =>{
